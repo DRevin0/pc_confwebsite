c4d41bbcd76cd276a86f057390f04e508b8d924b
DRevin0 <dimareva2000@gmail.com>
2026-02-26 20:18:13 +0300

    Structed and formatted Structed the previous commit Formatted maintenance

--- pc_configurator/main/urls.py
+++ pc_configurator/main/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.stub),
+    path('maintenance/', views.stub),
     path('about/', views.about, name='about'),
+    path('', views.homepage, name='homepage'),
 ] 
