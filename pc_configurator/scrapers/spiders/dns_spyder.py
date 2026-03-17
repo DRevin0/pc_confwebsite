e1eba829cbf03013595b4d898e4467a525f50dbf
DRevin0 <dimareva2000@gmail.com>
2026-03-17 21:46:53 +0300

    Add support all categories pc hardware Update README

--- pc_configurator/scrapers/spiders/dns_spyder.py
+++ pc_configurator/scrapers/spiders/dns_spyder.py
@@ -7,13 +7,26 @@ from scrapy_playwright.page import PageMethod
 class DnsSpider(scrapy.Spider):
     name = 'dns_spider'
     allowed_domains = ['dns-shop.ru']
-    start_urls = ['https://www.dns-shop.ru/catalog/17a899cd16404e77/processory/']
+
+    categories = {
+        'cpu': 'https://www.dns-shop.ru/catalog/17a899cd16404e77/processory/',
+        'motherboard':'https://www.dns-shop.ru/catalog/17a89a0416404e77/materinskie-platy/',
+        'gpu':'https://www.dns-shop.ru/catalog/17a89aab16404e77/videokarty/',
+        'ssd_sata':'https://www.dns-shop.ru/catalog/8a9ddfba20724e77/ssd-nakopiteli/',
+        'ssd_m2':'https://www.dns-shop.ru/catalog/dd58148920724e77/ssd-m2-nakopiteli/',
+        'ram':'https://www.dns-shop.ru/catalog/17a89a3916404e77/operativnaa-pamat-dimm/',
+        'psu':'https://www.dns-shop.ru/catalog/17a89c2216404e77/bloki-pitania/',
+        'air_cooling':'https://www.dns-shop.ru/catalog/17a9cc2d16404e77/kulery-dla-processorov/',
+        'liquid_cooling':'https://www.dns-shop.ru/catalog/17a9cc9816404e77/sistemy-zidkostnogo-ohlazdenia/',
+        'case':'https://www.dns-shop.ru/catalog/17a89c5616404e77/korpusa/',
+    }
+    start_urls = list(categories.values())
     MAX_PAGES = 1
-    MAX_ITEMS = 5
+    ITEMS_PER_CATEGORY = 3
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.items_scraped = 0
+        self.category_counts = {cat: 0 for cat in self.categories}
 
         cookies_path = Path(__file__).parent.parent / "cookies" / "dns_cookies.pkl"
         try:
@@ -46,11 +59,12 @@ class DnsSpider(scrapy.Spider):
             self.logger.error("Файлы cookie не найдены, завершение работы")
             return
 
-        for url in self.start_urls:
+        for cat, url in self.categories.items():
             yield scrapy.Request(
                 url,
                 callback=self.parse,
                 meta={
+                    'category':cat,
                     "playwright": True,
                     "playwright_context_kwargs": {
                         "storage_state": self.storage_state,  
@@ -65,18 +79,20 @@ class DnsSpider(scrapy.Spider):
     def parse(self, response):
         #self.logger.info(f"parse вызван для {response.url}")
         #self.logger.info(f"Статус ответа: {response.status}")
-        if self.items_scraped >= self.MAX_ITEMS:
+        category = response.meta['category']
+        current = self.category_counts[category]
+        if current >= self.ITEMS_PER_CATEGORY:
             return
         
         products = response.css('div.catalog-product')
         self.logger.info(f"Найдено товаров: {len(products)}")
         requests_sent = 0
 
-        page_number = response.meta.get('page', 1)
+#        page_number = response.meta.get('page', 1)
 
         for product in products:
-            if self.items_scraped + requests_sent >= self.MAX_ITEMS:
-                self.logger.info("Лимит товаров достигнут, останавливаем отправку новых запросов")
+            if current + requests_sent >= self.ITEMS_PER_CATEGORY:
+                self.logger.info(f"{category}, лимит товаров достигнут, останавливаем отправку новых запросов")
                 break
             product_url = product.css('a.catalog-product__name::attr(href)').get()
             #self.logger.info(f"Найдена ссылка: {product_url}")
@@ -85,6 +101,7 @@ class DnsSpider(scrapy.Spider):
                     product_url,
                     callback=self.parse_product,
                     meta={
+                        'category':category,
                         'playwright': True,
                         'playwright_page_methods': [
                             #PageMethod("wait_for_selector", "div.product-card-top_specs-item-title", timeout=30000),
@@ -95,26 +112,27 @@ class DnsSpider(scrapy.Spider):
                 )
                 requests_sent += 1
 
-        # Пагинация
-        if self.items_scraped + requests_sent < self.MAX_ITEMS: #page_number < self.MAX_PAGES
-            next_page = response.css('a.pagination-widget__page-link_next::attr(href)').get()
-            if next_page:
-                yield response.follow(
-                    next_page,
-                    callback=self.parse,
-                    meta={
-                        'playwright': True,
-                        'playwright_page_methods': [
-                            #PageMethod("add_cookies", self.playwright_cookies),  # на всякий случай
-                            PageMethod("wait_for_selector", "div.catalog-item"),
-                        ],
-                        'page': page_number + 1
-                    }
-                )
+        # Пагинация(на днс не используется)
+#        if self.items_scraped + requests_sent < self.MAX_ITEMS: #page_number < self.MAX_PAGES
+#            next_page = response.css('a.pagination-widget__page-link_next::attr(href)').get()
+#            if next_page:
+#                yield response.follow(
+#                    next_page,
+#                    callback=self.parse,
+#                    meta={
+#                        'playwright': True,
+#                        'playwright_page_methods': [
+#                            #PageMethod("add_cookies", self.playwright_cookies),  # на всякий случай
+#                            PageMethod("wait_for_selector", "div.catalog-item"),
+#                        ],
+#                        'page': page_number + 1
+#                    }
+#                )
 
     def parse_product(self, response):
-        if self.items_scraped >= self.MAX_ITEMS:
-            self.logger.debug("Лимит уже достигнут, пропускаем товар")
+        category = response.meta['category']
+        if self.category_counts[category] >= self.ITEMS_PER_CATEGORY:
+            self.logger.debug(f"{category}, лимит товаров достигнут, останавливаем отправку новых запросов")
             return
         name = response.css('h1.product-card-top__title::text').get()
         if not name:
@@ -136,17 +154,14 @@ class DnsSpider(scrapy.Spider):
             for title, value in zip(titles2, values2):
                 if title and value:
                     specs[title.strip().rstrip(':')] = value.strip()
-            self.logger.info(f"Найдено характеристик (вариант 2): {len(specs)}")
+            self.logger.info(f"Найдено характеристик: {len(specs)}")
         self.logger.debug(f"Характеристики: {specs}")
 
-        self.items_scraped += 1
+        self.category_counts[category] += 1
         yield {
             'name': name,
             'price': price,
-            'socket': specs.get('Сокет'),
-            'tdp': specs.get('Тепловыделение'),
-            'cores': specs.get('Количество ядер'),
+            **specs,
         }
-        if self.items_scraped >= self.MAX_ITEMS:
-            self.logger.info(f"Достигнут лимит товаров {self.MAX_ITEMS}, закрываем паука")
-            self.crawler.engine.close_spider(self, reason='items_limit_reached')
+        if self.category_counts[category] >= self.ITEMS_PER_CATEGORY:
+            self.logger.debug(f"{category}, лимит товаров достигнут, останавливаем отправку новых запросов")
